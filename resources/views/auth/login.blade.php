0196f1776aaf0ac5bb0b68178c1b4d6271e8884b
César González Magallanes <70915885+cesar1451@users.noreply.github.com>
2022-05-12 01:06:56 -0500

    Se agregó la verificación de email al registrarse

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+            </div>          
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
