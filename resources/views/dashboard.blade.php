f94e3d003d258a633c00acd88b0b79cfb6b3a3d1
Luis Fernando López Pacheco <luis.lopez0413@alumnos.udg.mx>
2022-05-12 18:33:31 -0500

    Modificación del navegador para aceptar rutas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
                     <a class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                        >
+                        href="{{ route('producto.create') }}">
                         Agregar Programa
                     </a>
                 </h4>
