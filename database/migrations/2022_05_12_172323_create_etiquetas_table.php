6370ed59ff835ac2db3daca925662560e6f62287
César González Magallanes <70915885+cesar1451@users.noreply.github.com>
2022-05-17 12:57:17 -0500

    Se agregó lo de SoftDeletes

--- database/migrations/2022_05_12_172323_create_etiquetas_table.php
+++ database/migrations/2022_05_12_172323_create_etiquetas_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('etiquetas', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->nullable();
+            $table->SoftDeletes();
             $table->timestamps();
         });
     }
